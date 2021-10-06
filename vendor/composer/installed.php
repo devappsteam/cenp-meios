4f0bccf30bc6e1cf694e75dd3656085d03182553
Caio Felipe <caiofelipe.up@gmail.com>
2021-10-06 18:18:19 -0300

    Add autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'worpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '746ca4e92e95bf758d396317bc469b2ba24a7edb',
+        'name' => 'devapps/cenp-meios',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'devapps/cenp-meios' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'worpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '746ca4e92e95bf758d396317bc469b2ba24a7edb',
+            'dev_requirement' => false,
+        ),
+    ),
+);
